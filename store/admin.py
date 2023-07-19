4eda085536a7abba03414948b14264b63b92be71
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-07-19 17:44:38 +0600

    computed columns

--- store/admin.py
+++ store/admin.py
@@ -6,9 +6,15 @@ from . import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
+
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
 
 
 admin.site.register(models.Collection)
