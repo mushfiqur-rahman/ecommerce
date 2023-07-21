7c3b122f43905db70957c9db9e6ca3fd4c768369
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-07-21 22:36:22 +0600

    customize product add form

--- store/admin.py
+++ store/admin.py
@@ -24,6 +24,10 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['collection']
+    prepopulated_fields = {
+        'slug': ['title']
+    }
     actions = ['clear_inventory']
     list_display = ['title', 'unit_price', 'inventory_status', 'collection__title']
     list_editable = ['unit_price']
@@ -53,6 +57,7 @@ class ProductAdmin(admin.ModelAdmin):
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
     list_display = ['title', 'products_count']
+    search_fields = ['title']
 
     @admin.display(ordering='products_count ')
     def products_count(self, collection):
