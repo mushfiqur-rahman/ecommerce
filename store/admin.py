b5aa11f2b4027bfaf1a82e56fb62b5a60aca6003
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-07-23 09:57:56 +0600

    product tag add

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin, messages
+from django.contrib.contenttypes.admin import GenericTabularInline
 from django.db.models import Count, QuerySet
 from django.urls import reverse
 from django.utils.html import format_html, urlencode
 
+from tags.models import TaggedItem
 from . import models
 
 
@@ -22,6 +24,11 @@ class InventoryFilter(admin.SimpleListFilter):
             return queryset.filter(inventory__lt=10)
 
 
+class TagInLine(GenericTabularInline):
+    autocomplete_fields = ['tag']
+    model = TaggedItem
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     autocomplete_fields = ['collection']
@@ -29,6 +36,7 @@ class ProductAdmin(admin.ModelAdmin):
         'slug': ['title']
     }
     actions = ['clear_inventory']
+    inlines = [TagInLine]
     list_display = ['title', 'unit_price', 'inventory_status', 'collection__title']
     list_editable = ['unit_price']
     list_filter = ['collection', 'last_update', InventoryFilter]
--- tags/models.py
+++ tags/models.py
@@ -5,13 +5,28 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type, obj_id):
+        content_type = ContentType.objects.get_for_model(obj_type)
+
+        return TaggedItem.objects \
+            .select_related('tag') \
+            .filter(
+            content_type=content_type,
+            object_id=obj_id
+        )
+
+
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.label
 
-class TagItem(models.Model):
+
+class TaggedItem(models.Model):
+    objects = TaggedItemManager()
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    # Type (product, video, article)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()  # all primary keys are PositiveIntegerField
+    object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
