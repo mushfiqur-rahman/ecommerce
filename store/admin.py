c5fa004f1de1be1426d84bb7e2bb279c54c915aa
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-07-23 07:47:30 +0600

    children inline

--- store/admin.py
+++ store/admin.py
@@ -34,6 +34,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
     list_select_related = ['collection']
+    search_fields = ['title']
 
     def collection__title(self, product):
         return product.collection.title
@@ -100,6 +101,16 @@ class CustomerAdmin(admin.ModelAdmin):
         )
 
 
+class OrderItemInline(admin.TabularInline):
+    autocomplete_fields = ['product']
+    min_num = 1
+    max_num = 10
+    model = models.OrderItem
+    extra = 0
+
+
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['customer']
+    inlines = [OrderItemInline]
     list_display = ['id', 'placed_at', 'customer']
