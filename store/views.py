973f69d238dbd9100cce5dfae1b971584b9e23bc
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-07-26 08:22:21 +0600

    collection detail add

--- store/views.py
+++ store/views.py
@@ -10,8 +10,10 @@ from .serializers import ProductSerializer
 # Create your views here.
 @api_view()
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
+    # queryset = Product.objects.all()
+    queryset = Product.objects.select_related('collection').all()
+    serializer = ProductSerializer(
+        queryset, many=True, context={'request':request})
     return Response(serializer.data)
 
 
@@ -20,3 +22,8 @@ def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    return Response('ok')
