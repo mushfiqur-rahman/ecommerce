afaa2dfebc563cf2b4a70b6c8ec7f7134afda919
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-08-21 11:54:26 +0600

    collection list show, update and delete

--- store/serializers.py
+++ store/serializers.py
@@ -6,12 +6,13 @@ from rest_framework import serializers
 class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
-        fields = ['id', 'title']
+        fields = ['id', 'title', 'products_count']
 
     """
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
 """
+    products_count = serializers.IntegerField()
 
 
 class ProductSerializer(serializers.ModelSerializer):
