314df75bcee68f52db8750111ecc3810b409350f
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-09-09 12:52:15 +0600

    drf nested router install & implement

--- store/views.py
+++ store/views.py
@@ -1,6 +1,4 @@
 from django.db.models.aggregates import Count
-from django.shortcuts import get_object_or_404
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
