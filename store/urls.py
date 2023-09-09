e12f75d4d00c9b35b301a4809078c8e67a075493
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-09-09 12:36:50 +0600

    drf nested router install & implement

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from . import views
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
 
-urlpatterns = router.urls
+urlpatterns = router.urls + products_router.urls
