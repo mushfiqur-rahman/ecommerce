5da0b898ff4446e80f22953edc9ac513ce8f6555
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-09-12 12:43:17 +0600

    collection_id filtering

--- store/views.py
+++ store/views.py
@@ -7,8 +7,14 @@ from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializ
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        collection_id = self.request.query_params.get('collection_id')
+        if collection_id is not None:
+            queryset = queryset.filter(collection_id=collection_id)
+        return queryset
 
     def get_serializer_context(self):
         return {'request': self.request}
@@ -33,7 +39,7 @@ class CollectionViewSet(ModelViewSet):
 
 class ReviewViewSet(ModelViewSet):
     serializer_class = ReviewSerializer
-    
+
     def get_queryset(self):
         return Review.objects.filter(product_id=self.kwargs['product_pk'])
 
