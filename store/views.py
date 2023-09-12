2413075dcecbddd2108edda4898cb29f90e3e1f4
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-09-12 17:19:54 +0600

    custom filters use

--- store/views.py
+++ store/views.py
@@ -3,6 +3,8 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 from django_filters.rest_framework import DjangoFilterBackend
+
+from .filters import ProductFilter
 from .models import Collection, Product, OrderItem, CartItem, Review
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
 
@@ -11,7 +13,7 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['collection_id']
+    filterset_class = ProductFilter
 
     def get_serializer_context(self):
         return {'request': self.request}
