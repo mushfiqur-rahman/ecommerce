15842094f9592eb3250219c4dd8ba4ecb46c0cc7
mushfiqur-rahman <mushfiq.style@gmail.com>
2023-10-17 09:10:04 +0600

    Designing the API

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import MinValueValidator
 from django.db import models
+from uuid import uuid4
 
 
 # Create your models here.
@@ -97,6 +98,7 @@ class Address(models.Model):
 
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
