cd5cbcd6f0dd5599bc63d248b7c7f793394f35ad
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-01-07 15:35:46 +0600

    order specify for admin only

--- store/views.py
+++ store/views.py
@@ -109,7 +109,12 @@ class CustomerViewSet(ModelViewSet):
 
 
 class OrderViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'patch', 'delete', 'head', 'options']
+
+    def get_permissions(self):
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
 
     def create(self, request, *args, **kwargs):
         serializer = CreateOrderSerializer(
