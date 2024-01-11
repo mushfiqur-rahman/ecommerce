2766cb7776ef68c816d05051165e49ad00b42f5b
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-01-11 12:26:33 +0600

    managing media files

--- core/settings/base.py
+++ core/settings/base.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -155,6 +156,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
