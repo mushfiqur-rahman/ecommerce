505dfc01ded8a31a9b027585aaa88e51e9c047eb
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-01-13 09:36:48 +0600

    image reg in admin and css for admin added

--- main/admin.py
+++ main/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
 
-from store.admin import ProductAdmin
+from store.admin import ProductAdmin, ProductImageInline
 from store.models import Product
 from tags.models import TaggedItem
 from .models import User
@@ -28,7 +28,7 @@ class TagInLine(GenericTabularInline):
 
 
 class CustomProductAdmin(ProductAdmin):
-    inlines = [TagInLine]
+    inlines = [TagInLine, ProductImageInline]
 
 
 admin.site.unregister(Product)
