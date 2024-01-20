298b8323af2e101e2c2cd319f5b731f2f9b45cd4
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-01-20 21:34:27 +0600

    admin auth test

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -14,3 +14,14 @@ class TestCreateCollection:
 
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_admin_returns_403(self):
+        # A: Arrange
+
+        # A: Act
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        # Assert
+        assert response.status_code == status.HTTP_403_FORBIDDEN
