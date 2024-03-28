8424ecd3f5ddc1d4b28b1152bc5bf0272347fbde
mushfiqur-rahman <mushfiq.style@gmail.com>
2024-03-28 08:03:48 +0600

    pytest AssertionError fixed

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,4 +1,4 @@
-from store.models import Collection
+from store.models import Collection, Product
 from rest_framework import status
 import pytest
 from model_bakery import baker
@@ -7,52 +7,37 @@ from model_bakery import baker
 @pytest.fixture
 def create_collection(api_client):
     def do_create_collection(collection):
-        return api_client.post('/store/collections', collection)
-
+        return api_client.post('/store/collections/', collection)
     return do_create_collection
 
 
 @pytest.mark.django_db
 class TestCreateCollection:
     def test_if_user_is_anonymous_returns_401(self, create_collection):
-        # A: Arrange
-
-        # A: Act
         response = create_collection({'title': 'a'})
 
-        # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     def test_if_user_is_not_admin_returns_403(self, authenticate, create_collection):
         authenticate()
-        # A: Arrange
-
-        # A: Act
 
         response = create_collection({'title': 'a'})
 
-        # Assert
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
     def test_if_data_is_invalid_returns_400(self, authenticate, create_collection):
         authenticate(is_staff=True)
-        # A: Arrange
 
-        # A: Act
         response = create_collection({'title': ''})
-        # Assert
+
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
 
     def test_if_data_is_valid_returns_201(self, authenticate, create_collection):
         authenticate(is_staff=True)
-        # A: Arrange
-
-        # A: Act
 
         response = create_collection({'title': 'a'})
 
-        # Assert
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
 
@@ -60,7 +45,6 @@ class TestCreateCollection:
 @pytest.mark.django_db
 class TestRetrieveCollection:
     def test_if_collection_exists_returns_200(self, api_client):
-        # Arrange
         collection = baker.make(Collection)
 
         response = api_client.get(f'/store/collections/{collection.id}/')
